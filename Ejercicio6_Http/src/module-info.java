969b6476f438fc400160a320e0ac0d567471ae87
ibrebollo <35426667+ibrebollo@users.noreply.github.com>
2023-06-20 19:23:21 +0200

    Delete Ejercicio6_Http directory

--- Ejercicio6_Http/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.viewnext.ejercicio6 {
-	requires java.net.http;
-<<<<<<< HEAD
-	requires org.json;
-=======
->>>>>>> branch 'master' of https://github.com/ibrebollo/Java-9-11-Novedades-y-Buenas-Practicas.git
-	
-}
