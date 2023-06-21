28224b3e8645cbbd0d8c7a78bf77c30866189c3b
Inma B.R <0007897@PC0KQV2Q.mshome.net>
2023-06-21 15:19:02 +0200

    Completo comentarios

--- Ejemplo10_Try_with_resources/src/com/viewnext/AppMain.java
+++ Ejemplo10_Try_with_resources/src/com/viewnext/AppMain.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class AppMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception { // (*)
 		
 		// Antes
 		Connection con = null; 
@@ -24,11 +24,20 @@ public class AppMain {
 		}
 		
 		
-		// A partir de Java 9 no se necesita el finally
-		try (Connection con2 = DriverManager.getConnection("", "", "")) {
-			
+		// A partir de Java 9, el try con recursos no se necesita el finally
+		try (Connection con2 = DriverManager.getConnection("", "", "")) {// En este caso NO es necesario declarar el throws Exception
+			// Cuando termina el try, cierra la conexión automáticamente, y si salta la excepción....
 		}catch(Exception e) {
-			
+			// ... también la cierra aquí. 	
+		}
+		
+		
+		// Opción con variable: A partir de Java 9, el try con recursos no se necesita el finally
+		Connection con3 = DriverManager.getConnection("", "", ""); // En este caso es necesario declarar el throws Exception (*)
+		try (con3) {
+			// Cuando termina el try, cierra la conexión automáticamente, y si salta la excepción....
+		}catch(Exception e) {
+			// ... también la cierra aquí. 	
 		}
 		
 	}
