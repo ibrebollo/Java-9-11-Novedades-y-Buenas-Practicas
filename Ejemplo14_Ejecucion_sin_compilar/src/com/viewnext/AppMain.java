1c28187387eb1ce51c192f9055fbd06627ab15a5
Inma B.R <0007897@PC0KQV2Q.mshome.net>
2023-06-21 17:45:35 +0200

    Ejemplo 14: ejecución sin compilación
    Desde linea de comandos no se hace primero un javac, sino directamente
    un java C:\Workspace\Java-9-11-Novedades-y-Buenas-Practicas-19-22-Junio-Viewnext\Ejemplo14_Ejecucion_sin_compilar\src\com\viewnext\AppMain.java
    
    Y se ejecuta sin necesidad de compilación previa

---
+++ Ejemplo14_Ejecucion_sin_compilar/src/com/viewnext/AppMain.java
@@ -0,0 +1,14 @@
+package com.viewnext;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// A partir de java 11 se puede ejecutar sin compilar. 
+		// Solo en la clase principal main.
+		
+		System.out.println("Ejecución sin compilación desde línea de comandos con comando: "
+				+ "java C:\\Workspace\\Java-9-11-Novedades-y-Buenas-Practicas-19-22-Junio-Viewnext\\Ejemplo14_Ejecucion_sin_compilar\\src\\com\\viewnext\\AppMain.java");
+
+	}
+
+}
